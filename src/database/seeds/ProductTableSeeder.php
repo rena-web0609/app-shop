a5fc907e91df62cdaf06b5198333dc5b13264ef1
Rena <choco_rena0609@yahoo.co.jp>
2019-06-30 16:22:37 +0900

    create seeder(product)

--- src/database/seeds/ProductTableSeeder.php
+++ src/database/seeds/ProductTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class ProductTableSeeder extends Seeder
 {
@@ -11,6 +12,15 @@ class ProductTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('products')->insert([
+            'name' => 'ドットノースリプルオーバー',
+            'comment' => '去年の夏に大人気で完売していた商品が数量限定で待望の再入荷！！今季トレンドのワッフル素材にドット柄が登場。スタイルが良く見えるフレンチスリーブのTシャツです。首つまりのデザインも今年顔の１枚。春はカーデやシャツのインナー、夏は１枚でメインに着てもさらっと着て頂けます。',
+            'price' => '3780',
+            'category_id' => 1,
+            'pic' => 'a',
+            'user_id' => 1,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
     }
 }
