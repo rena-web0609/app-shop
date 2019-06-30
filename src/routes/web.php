75584dc77c4a4d2c7edefdcb4ef58998cae8545c
Rena <choco_rena0609@yahoo.co.jp>
2019-06-30 17:00:46 +0900

    modify (500)

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-//Route::get('products','ProductController');
+Route::get('products', 'ProductController@index')->name('products.index');
+// Route::get('products/create', 'PlanController@create')->name('products.create');
+// Route::post('products', 'PlanController@store')->name('products.store');
+// Route::get('products/{product}', 'PlanController@show')->name('products.show');
+// Route::get('products/{product}/edit', 'PlanController@edit')->name('products.edit');
+// Route::put('products/{product}', 'PlanController@update')->name('products.update');
+// Route::delete('products/{product}', 'PlanController@destroy')->name('products.destroy');
