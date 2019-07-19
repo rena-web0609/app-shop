5b6a033aa9043b81cbd272fa9fdac421d4869a6b
Rena <choco_rena0609@yahoo.co.jp>
2019-07-19 13:29:25 +0900

    seeder

--- src/database/migrations/2019_06_29_165658_create_products_table.php
+++ src/database/migrations/2019_06_29_165658_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -23,6 +24,62 @@ class CreateProductsTable extends Migration
             $table->unsignedBigInteger('user_id');
             $table->timestamps();
         });
+
+        DB::table('products')->insert([
+            'name' => 'ニット',
+            'comment' => 'シンプルなニットです。',
+            'price' => '3780',
+            'pic' => 'a',
+            'user_id' => '1',
+            'category_id' => '0',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        DB::table('products')->insert([
+            'name' => 'Yシャツ',
+            'comment' => 'シンプルなYシャツです。',
+            'price' => '3780',
+            'pic' => 'a',
+            'user_id' => '1',
+            'category_id' => '1',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        DB::table('products')->insert([
+            'name' => 'スニーカー',
+            'comment' => '子供用のスニーカーです。',
+            'price' => '3780',
+            'pic' => 'a',
+            'user_id' => '1',
+            'category_id' => '2',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+
+        DB::table('products')->insert([
+            'name' => 'ノースリプルオーバー',
+            'comment' => 'スタイルが良く見えるフレンチスリーブのTシャツです。首つまりのデザインも今年顔の１枚。',
+            'price' => '3780',
+            'pic' => 'a',
+            'user_id' => '2',
+            'category_id' => '0',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        DB::table('products')->insert([
+            'name' => 'アンゴラニット',
+            'comment' => 'ふんわりとしたニットです。',
+            'price' => '3780',
+            'pic' => 'a',
+            'user_id' => '3',
+            'category_id' => '1',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
     }
 
     /**
