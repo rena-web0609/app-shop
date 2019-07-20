14d111f18190b3d33520ee3d8fd9a2fce61f2cc2
Rena <choco_rena0609@yahoo.co.jp>
2019-07-20 17:34:03 +0900

    DB

--- src/database/migrations/2019_06_29_165658_create_products_table.php
+++ src/database/migrations/2019_06_29_165658_create_products_table.php
@@ -20,7 +20,7 @@ class CreateProductsTable extends Migration
             $table->text('comment')->nullable();
             $table->integer('price')->nullable();
             $table->unsignedBigInteger('category_id')->nullable();
-            $table->text('pic')->nullable();
+            $table->string('pic', 255)->nullable();
             $table->unsignedBigInteger('user_id')->nullable();
             $table->timestamps();
         });
